cc52f12e3a03845508ead270c67d00a0a2de2b31
Ayushman Gupta <guptaayushman24@gmail.com>
2025-05-19 01:27:22 +0530

    Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import {prisma} from '../lib/prisma'
 export default async function Home() {
   const userschema = await prisma.user.findMany({})
+  console.log(userschema);
   return(
     userschema.map((user)=>(
       <div key={user.id}>{user.name}</div>
